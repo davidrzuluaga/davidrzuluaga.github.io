f2ae1a34b1aff15a56d39643f9536a9dcea9e056
DavidRZuluaga <git@davidrzuluaga.com>
2018-12-01 12:22:26 -0500

    Updates

---
+++ precache-manifest.95a1c67d24fcda3fae11ffc0f3740c84.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe05078528735805b692bbd256b9a36b",
+    "url": "/static/media/guessnumber.fe050785.png"
+  },
+  {
+    "revision": "ef888b8806547e7e3a2a",
+    "url": "/static/css/main.b8646874.chunk.css"
+  },
+  {
+    "revision": "614da4bcb198e3fe417b",
+    "url": "/static/js/1.614da4bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6121ab6d8452766a51e6eceb5026c543",
+    "url": "/static/media/skills.6121ab6d.jpg"
+  },
+  {
+    "revision": "131d47776443f34c92706760652cfd2f",
+    "url": "/static/media/NYC-path.131d4777.jpg"
+  },
+  {
+    "revision": "706a16b2f1ab533768d340dfb35cc3d7",
+    "url": "/static/media/DRZpic(mobile).706a16b2.jpg"
+  },
+  {
+    "revision": "76b68ef4d4c10b6900ee818ba54a7263",
+    "url": "/static/media/DRZpic.76b68ef4.jpg"
+  },
+  {
+    "revision": "af781e8a6e0f9628d29e945ea754bef8",
+    "url": "/static/media/Expensify.af781e8a.png"
+  },
+  {
+    "revision": "322e44361b1cf792e6cb4f48f4d319bf",
+    "url": "/static/media/Flow_Overstack.322e4436.png"
+  },
+  {
+    "revision": "524b1e7962602e53a46678104e1482ed",
+    "url": "/static/media/blog.524b1e79.png"
+  },
+  {
+    "revision": "ef888b8806547e7e3a2a",
+    "url": "/static/js/main.ef888b88.chunk.js"
+  },
+  {
+    "revision": "2b8da3067ac7ef7c85636b81056c48bc",
+    "url": "/static/media/postspopulares.2b8da306.png"
+  },
+  {
+    "revision": "3e139ba267ee8ece16396e91fac809db",
+    "url": "/static/media/timers.3e139ba2.png"
+  },
+  {
+    "revision": "cbf9cf797597c8ab774dc235607b8afe",
+    "url": "/static/media/CSS3_and_HTML5_logos_and_wordmarks.svg.cbf9cf79.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/JavaScript-logo.0cded3a3.png"
+  },
+  {
+    "revision": "8e26f22094a11f6a689d8302dc30782c",
+    "url": "/static/media/React.8e26f220.svg"
+  },
+  {
+    "revision": "b882387a5a2b494c2c200e66d63fa8db",
+    "url": "/static/media/Redux.b882387a.png"
+  },
+  {
+    "revision": "b9f77baf3ecc0ddc34a1d843c973efda",
+    "url": "/static/media/Ruby_On_Rails_Logo.b9f77baf.png"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/static/media/bootstrap-stack.73844123.png"
+  },
+  {
+    "revision": "4c5516d02a86ccf4b4f774ead242cda8",
+    "url": "/static/media/jQuery.4c5516d0.png"
+  },
+  {
+    "revision": "9467ef6b7bc5c59cd832003cf1ec348a",
+    "url": "/index.html"
+  }
+];
